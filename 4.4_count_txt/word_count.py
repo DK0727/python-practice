275a93d19956a24a259e5263b6ed06705f9c474e
dukai <123@qq.com>
2026-04-05 14:54:54 +0800

    封装统计函数，增加异常处理

--- 4.4_count_txt/word_count.py
+++ 4.4_count_txt/word_count.py
@@ -18,3 +18,5 @@ if __name__ == '__main__':
     with open('result.txt','w',encoding='utf-8') as f:
         for word ,count in word_count.items():
             f.write(f"{word}{count}\n")
+
+
---
+++ 4.5_count_txt_tool/word_count.py
@@ -0,0 +1,53 @@
+import re
+# if __name__ == '__main__':
+#     with open('sample.txt','r',encoding='utf-8') as f:
+#         content = f.read()
+#         content= content.lower()
+#         print(content)
+#     content=re.sub(r'[^a-z0-9\s]',' ',content)
+#     print(content)
+#
+#     words = content.split()
+#     print(words)
+#
+#     word_count={}
+#     for word in words:
+#         word_count[word]=word_count.get(word,0) +1
+#     print(word_count)
+#
+#     with open('result.txt','w',encoding='utf-8') as f:
+#         for word ,count in word_count.items():
+#             f.write(f"{word}{count}\n")
+
+
+
+
+def count_words(input_file,output_file):
+    try:
+        with open(input_file,'r',encoding='utf-8') as f:
+            content = f.read()
+            content = content.lower()
+    except FileNotFoundError:
+        print(f'未找到{input_file}文件')
+        return
+    except UnicodeDecodeError:
+        print(f'{input_file}文件的格式不正确请转为utf-8格式')
+    content = re.sub(r'[^a-z0-9\s]', ' ', content)
+    print(content)
+
+    words = content.split()
+    print(words)
+
+    word_count = {}
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+    print(word_count)
+
+    with open(output_file, 'w', encoding='utf-8') as f:
+        for word, count in word_count.items():
+            f.write(f"{word} {count}\n")
+
+if __name__ == '__main__':
+    count_words('sample.txt','result.txt')
+
+
